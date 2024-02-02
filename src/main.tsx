210c2f2e1c2a250237c382d9b9429a5329951d84
Kazte <39470734+Kazte@users.noreply.github.com>
2024-02-02 10:41:10 -0300

    🍞 - add Toaster component to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import { ThemeProvider } from './components/providers/theme-provider';
+import { Toaster } from './components/ui/toaster';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme='dark'>
       <App />
+      <Toaster />
     </ThemeProvider>
   </React.StrictMode>
 );
