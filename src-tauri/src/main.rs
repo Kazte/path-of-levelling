785720cb8248b3a5697121d7f44fd555d5844218
Kazte <39470734+Kazte@users.noreply.github.com>
2024-02-02 17:16:12 -0300

    ✨ - update window resizable property

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,29 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{CustomMenuItem, Menu, Submenu};
-
 #[tokio::main]
 async fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let close = CustomMenuItem::new("close".to_string(), "Close");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-    let menu = Menu::new()
-        // .add_native_item(MenuItem::Zoom)
-        // .add_item(CustomMenuItem::new("hide", "Hide"))
-        .add_submenu(submenu);
-
     tauri::Builder::default()
-        .menu(menu)
-        .on_menu_event(|event| match event.menu_item_id() {
-            "quit" => {
-                std::process::exit(0);
-            }
-            "close" => {
-                std::process::exit(0);
-            }
-            _ => {}
-        })
         .invoke_handler(tauri::generate_handler![get_area_name])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
