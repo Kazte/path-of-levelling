9205388b3308a8b1fbe669a79a11f13afbb3cd15
Kazte <39470734+Kazte@users.noreply.github.com>
2024-03-29 02:40:01 -0300

    Add settings page with client.txt path functionality

--- src/pages/settings.page.tsx
+++ src/pages/settings.page.tsx
@@ -1,3 +1,72 @@
+import { Check, X } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Menubar } from '@/components/ui/menubar';
+import { open } from '@tauri-apps/api/dialog';
+import { useNavigate } from 'react-router-dom';
+import { useSettingsStore } from '@/store/settings.store';
+import { useState } from 'react';
+
 export default function SettingsPage() {
-  return <div>settings.page</div>;
+  const navigator = useNavigate();
+  const { clientTxtPath, setClientTxtPath } = useSettingsStore(
+    (state) => state
+  );
+
+  const [clientTxtPathValue, setClientTxtPathValue] = useState(clientTxtPath);
+
+  const handleOnClose = () => {
+    navigator('/');
+  };
+
+  const handleSetClientTxt = async () => {
+    const selection = await open({
+      multiple: false,
+      filters: [{ name: 'Text', extensions: ['txt'] }]
+    });
+
+    if (selection) {
+      setClientTxtPath(selection as string);
+      setClientTxtPathValue(selection as string);
+    }
+  };
+
+  return (
+    <>
+      <Menubar
+        className='rounded-none border-0 border-b-[2px] px-2 lg:px-4 justify-between h-[35px]'
+        data-tauri-drag-region
+      >
+        <h4 className='select-none' data-tauri-drag-region>
+          Settings
+        </h4>
+        <div className='flex flex-row gap-2 justify-center items-center'>
+          <Button className='h-6' size='sm' onClick={handleOnClose}>
+            <Check size={20} /> Apply
+          </Button>
+        </div>
+      </Menubar>
+      <main className='flex-grow p-2 overflow-y-auto'>
+        <section className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-1'>
+            <Label>Client.txt Path</Label>
+            <div className='flex w-full max-w-sm items-center space-x-2'>
+              <Input
+                type='text'
+                placeholder='Client.txt path'
+                value={clientTxtPathValue}
+                onChange={(e) => setClientTxtPathValue(e.target.value)}
+                onBlur={() => setClientTxtPath(clientTxtPathValue)}
+              />
+              <Button type='button' onClick={handleSetClientTxt}>
+                Find File
+              </Button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
