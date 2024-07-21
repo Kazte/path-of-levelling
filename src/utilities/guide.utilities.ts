8da7c93def353cb943e1e711f1903ea72a778ccc
Kazte <castellittofranco1@gmail.com>
2024-07-21 04:15:35 -0300

    ✨ - add gem fragment support

--- src/utilities/guide.utilities.ts
+++ src/utilities/guide.utilities.ts
@@ -4,19 +4,16 @@ import {
   IStepImport
 } from '../interfaces/guide-import.interface';
 
-import Ajv from 'ajv';
+import { Gem } from '@/interfaces/types';
 import { areas } from '@/data/level-tracker-areas';
-import { guideSchema } from './guide.schema';
+import { gems } from '@/data/level-tracker-gems';
 import { quests } from '@/data/level-tracker-quests';
 import { useGuideStore } from '@/store/guide.store';
-
-const avj = new Ajv();
 
 export function sanitizeGuide(rawGuide: IGuideImport): IGuide {
   const flatGuide: IStepImport[] = [...rawGuide]
     .map((section) => section.steps)
     .flat();
-  console.log('Flat Guide: ', flatGuide);
 
   let guide: IGuide = [];
 
@@ -29,96 +26,113 @@ export function sanitizeGuide(rawGuide: IGuideImport): IGuide {
     let isEnterStep = false;
     let changeAreaId = '';
 
-    step.parts.forEach((part: any) => {
-      // Sanitize Parts (Each part of a step)
-      if (typeof part === 'object') {
-        switch (part.type) {
-          case 'enter':
-            // @ts-ignore
-            const area = areas[part.areaId];
-            sanitizedSubstepDescription += area.name;
-
-            isEnterStep = true;
-            changeAreaId = part.areaId;
-
-            break;
-          case 'kill':
-            sanitizedSubstepDescription += part.value;
-            break;
-          case 'arena':
-            sanitizedSubstepDescription += part.value;
-            break;
-          case 'area':
-            // @ts-ignore
-            sanitizedSubstepDescription += areas[part.areaId].name;
-            break;
-          case 'quest':
-            const quest = part.questId;
-
-            // @ts-ignore
-            const rewardOffers = quests[quest].reward_offers[quest];
-
-            if (rewardOffers) {
-              sanitizedSubstepDescription += `${rewardOffers.quest_npc}.`;
-            } else {
+    if (step.type === 'fragment_step') {
+      step.parts.forEach((part: any) => {
+        // Sanitize Parts (Each part of a step)
+        if (typeof part === 'object') {
+          switch (part.type) {
+            case 'enter':
               // @ts-ignore
-              sanitizedSubstepDescription = `Complete ${quests[quest].name}`;
-            }
-            break;
-          case 'waypoint_use':
-            // @ts-ignore
-            const dstArea = areas[part.dstAreaId].name;
-            // @ts-ignore
-            const srcArea = areas[part.srcAreaId].name;
-            // sanitizedSubstepDescription += `Use waypoint from ${srcArea} to ${dstArea}.`;
-            sanitizedSubstepDescription += `Use waypoint to ${dstArea}.`;
-            isEnterStep = true;
-            changeAreaId = part.dstAreaId;
-            break;
-          case 'portal_set':
-            // @ts-ignore
-            sanitizedSubstepDescription += 'portal';
-            break;
-          case 'portal_use':
-            // @ts-ignore
-            const dstAreaPortal = areas[part.dstAreaId].name;
-            sanitizedSubstepDescription += `portal to ${dstAreaPortal}.`;
-            break;
-          case 'waypoint_get':
-            sanitizedSubstepDescription += `waypoint`;
-            break;
-          case 'logout':
-            sanitizedSubstepDescription += 'Logout.';
-            isEnterStep = true;
-            changeAreaId = part.areaId;
-            break;
-          case 'trial':
-            sanitizedSubstepDescription += 'Trial';
-            break;
-          case 'crafting':
-            sanitizedSubstepDescription += 'crafting';
-            break;
-          case 'ascend':
-            sanitizedSubstepDescription += `Complete the ${part.version}_lab`;
-            break;
-          case 'waypoint':
-            sanitizedSubstepDescription += 'waypoint';
-            break;
-          case 'generic':
-            sanitizedSubstepDescription += part.value;
-            break;
-          case 'dir':
-            sanitizedSubstepDescription += dirIndex[part.dirIndex];
-            break;
-          default:
-            sanitizedSubstepDescription +=
-              part.value || 'PART NOT FOUND: ' + part.type;
-            break;
+              const area = areas[part.areaId];
+              sanitizedSubstepDescription += area.name;
+
+              isEnterStep = true;
+              changeAreaId = part.areaId;
+
+              break;
+            case 'kill':
+              sanitizedSubstepDescription += part.value;
+              break;
+            case 'arena':
+              sanitizedSubstepDescription += part.value;
+              break;
+            case 'area':
+              // @ts-ignore
+              sanitizedSubstepDescription += areas[part.areaId].name;
+              break;
+            case 'quest':
+              const quest = part.questId;
+
+              // @ts-ignore
+              const rewardOffers = quests[quest].reward_offers[quest];
+
+              if (rewardOffers) {
+                sanitizedSubstepDescription += `${rewardOffers.quest_npc}.`;
+              } else {
+                // @ts-ignore
+                sanitizedSubstepDescription = `Complete ${quests[quest].name}`;
+              }
+              break;
+            case 'waypoint_use':
+              // @ts-ignore
+              const dstArea = areas[part.dstAreaId].name;
+              // @ts-ignore
+              const srcArea = areas[part.srcAreaId].name;
+              // sanitizedSubstepDescription += `Use waypoint from ${srcArea} to ${dstArea}.`;
+              sanitizedSubstepDescription += `Use waypoint to ${dstArea}.`;
+              isEnterStep = true;
+              changeAreaId = part.dstAreaId;
+              break;
+            case 'portal_set':
+              // @ts-ignore
+              sanitizedSubstepDescription += 'portal';
+              break;
+            case 'portal_use':
+              // @ts-ignore
+              const dstAreaPortal = areas[part.dstAreaId].name;
+              sanitizedSubstepDescription += `portal to ${dstAreaPortal}.`;
+              break;
+            case 'waypoint_get':
+              sanitizedSubstepDescription += `waypoint`;
+              break;
+            case 'logout':
+              sanitizedSubstepDescription += 'Logout.';
+              isEnterStep = true;
+              changeAreaId = part.areaId;
+              break;
+            case 'trial':
+              sanitizedSubstepDescription += 'Trial';
+              break;
+            case 'crafting':
+              sanitizedSubstepDescription += 'crafting';
+              break;
+            case 'ascend':
+              sanitizedSubstepDescription += `Complete the ${part.version}_lab`;
+              break;
+            case 'waypoint':
+              sanitizedSubstepDescription += 'waypoint';
+              break;
+            case 'generic':
+              sanitizedSubstepDescription += part.value;
+              break;
+            case 'dir':
+              sanitizedSubstepDescription += dirIndex[part.dirIndex];
+              break;
+            default:
+              sanitizedSubstepDescription +=
+                part.value || 'PART NOT FOUND: ' + part.type;
+              break;
+          }
+        } else {
+          sanitizedSubstepDescription += part;
         }
+      });
+    } else if (step.type === 'gem_step') {
+      // @ts-ignore
+      const gemId = step.requiredGem.id;
+      const gem = gems.find((g: Gem) => g.id === gemId) as Gem;
+
+      if (gem) {
+        sanitizedSubstepDescription += `Get gem ${gem.name}`;
       } else {
-        sanitizedSubstepDescription += part;
+        const gemNameRaw = gemId.split('/')[gemId.split('/').length - 1];
+        sanitizedSubstepDescription += `Get gem ${gemNameRaw}`;
       }
-    });
+
+      // const gem = gems.find((g) => g.id === gemId);
+
+      // sanitizedSubstepDescription += `Get gem ${gem}`;
+    }
 
     sanitizedSubsteps.push({
       description: sanitizedSubstepDescription
@@ -148,10 +162,10 @@ export function setNewGuide(guideText: string) {
 
   try {
     const guideData = JSON.parse(guideText);
-    const validate = avj.compile(guideSchema);
-    if (!validate(guideData)) {
-      throw new Error('Invalid guide data');
-    }
+    // const validate = avj.compile(guideSchema);
+    // if (!validate(guideData)) {
+    //   throw new Error('Invalid guide data');
+    // }
     useGuideStore.setState({
       guide: sanitizeGuide(guideData as IGuideImport),
       currentStep: 0
